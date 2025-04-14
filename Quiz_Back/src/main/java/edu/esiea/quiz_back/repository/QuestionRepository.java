a42a27cb5ebc5b39fee72c05640dd4a5b8883793
ingivre <besseau@et.esiea.fr>
2025-04-14 11:31:44 +0200

    ⚡️ Add repositories

--- Quiz_Back/src/main/java/edu/esiea/quiz_back/repository/QuestionRepository.java
+++ Quiz_Back/src/main/java/edu/esiea/quiz_back/repository/QuestionRepository.java
@@ -1,4 +1,11 @@
 package edu.esiea.quiz_back.repository;
 
-public interface QuestionRepository {
+import edu.esiea.quiz_back.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
 }
+
--- Quiz_Back/src/main/java/edu/esiea/quiz_back/repository/QuizRepository.java
+++ Quiz_Back/src/main/java/edu/esiea/quiz_back/repository/QuizRepository.java
@@ -1,4 +1,10 @@
 package edu.esiea.quiz_back.repository;
 
-public interface QuizRepository {
+import edu.esiea.quiz_back.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+
 }

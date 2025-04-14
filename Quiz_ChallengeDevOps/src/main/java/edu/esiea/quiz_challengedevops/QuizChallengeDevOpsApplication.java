7927bfa02971c72c1629ba96a420264400b1022e
ingivre <besseau@et.esiea.fr>
2025-04-14 09:19:24 +0200

    ✨ Create new back Project ✨

---
+++ Quiz_ChallengeDevOps/src/main/java/edu/esiea/quiz_challengedevops/QuizChallengeDevOpsApplication.java
@@ -0,0 +1,13 @@
+package edu.esiea.quiz_challengedevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizChallengeDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizChallengeDevOpsApplication.class, args);
+	}
+
+}
---
+++ Quiz_ChallengeDevOps/src/test/java/edu/esiea/quiz_challengedevops/QuizChallengeDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esiea.quiz_challengedevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizChallengeDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
